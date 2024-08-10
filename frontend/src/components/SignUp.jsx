0b7bf34bc98ebd90bb6ae963dbd112d55a8ab46e
unknown <sv129899@gmail.com>
2024-08-10 13:29:22 +0530

    signup fix

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -20,7 +20,7 @@ const SignUp = () => {
   const onSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://helpify-backend-7a73.onrender.com/auth/signup', formData, {
+      const res = await axios.post('https://helpify-backend-7a73.onrender.com/api/auth/signup', formData, {
         headers: {
           'Content-Type': 'application/json'
         }
